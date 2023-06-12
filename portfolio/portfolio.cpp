1cb527ddf6504e9d6712f3255e92068acc277b02
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-06-12 14:08:21 +0900

    초기 커밋.

---
+++ portfolio/portfolio.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <thread>
+
+void syncPerformTask() {//sync 동기 코드
+	std::cout << "작업 수행중 ..." << std::endl;
+}
+
+void asyncPerformTask() {
+	for (int i = 0; i < 100; i++) {
+
+		std::cout << i << std::endl;
+	}
+}
+
+
+
+int main(void) {
+
+	std::cout << "10" << std::endl;
+	std::cout << "시작" << std::endl;
+	asyncPerformTask();
+	std::cout << "종료" << std::endl;
+	return 0;
+
+}
