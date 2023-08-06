ea9028421b5c5aaf016447ea24c5c56fa4075a22
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-08-07 00:16:33 +0900

    Update server.cpp

--- portfolio/server.cpp
+++ portfolio/server.cpp
@@ -1,10 +1,12 @@
-﻿// testprojeect.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// testprojeect.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+
 
 #include "pch.h"
 #include <iostream>
 #include <stdint.h>
 #include <vector>
+#include<WS2tcpip.h>
 #pragma comment(lib, "ws2_32")
 
 #define PORT	4578// 예약된 포트를 제외하고 사용해야함  (ex) 21 : FTP포트, 80 : HTTP포트, 8080 : HTTPS포트)
@@ -17,6 +19,14 @@ using namespace std;
 SOCKET hListen, hClient;
 u_long nonBlockingMode = 1;//굳이 1이 아니더라도 괜찮음 0이면 블로킹 나머지는 논블로킹임
 
+struct Info {//server와 client두개가 같아야함
+	string name;
+	string content;
+	int year;
+	int month;
+	int day;
+};
+
 void func1() {// 연결과 동시에 작동 되어있음
 	for (int i = 0; i < 10; i++) {
 		cout << "쓰레드 함수 작동중" << endl;
@@ -27,7 +37,7 @@ void func1() {// 연결과 동시에 작동 되어있음
 /*void proc_recvs() { // 해당함수 사용 안 하는중
 	char buff[PACKET_SIZE] = { 0 };
 	//string cmd;
-	
+
 	while (!WSAGetLastError()) {
 		cout << "연결성공\n";
 		ZeroMemory(&buff, PACKET_SIZE);//ZeroMemory는 함수가 아닌 매크로
@@ -45,9 +55,12 @@ int main() {
 	WSADATA wsaData;// 윈도우 소켓 초기화 정보 저장하기 위한 구조체 이미 선언되어있음
 	WSAStartup(MAKEWORD(2, 2), &wsaData);//WSAStartup(소켓버전, WSADATA 구조체 주소); 인데 MAKEWORD를 통해서 정수값으로 변환해서 넣어줌 2번째는 WSADATA의 구조체 포인터 타입
 
-	
+	Info rinfo;
+
 	int ip_addr = 0;
 
+	string username = "";
+	int year = 0;
 	//SOCKET hListen;
 	hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);//PF_INET IPV4타입 사용 일반적으로 사용하는 주소, SOCK_STREAM 연결지향형 소켓을 만든다 세번째 인자 protocoldms 통신규약 현재는 IPROTO_TCP로 TCP를 사용한다느 말
 	if (ioctlsocket(hListen, FIONBIO, &nonBlockingMode) == INVALID_SOCKET) {//SOCKET socket 실패시 INVALD_SOCKET 리턴 즉 ioctlsocket
@@ -67,22 +80,32 @@ int main() {
 	int iClntSize = sizeof(tClntAddr);
 	thread t1(func1);
 
-	
+
 
 	while (true) {
 		hClient = accept(hListen, (SOCKADDR*)&tClntAddr, &iClntSize);//accept(소켓, 소켓 구성요소 구조체 주소,그 구조체를 담고있는 별수 크기
 
 		//ip_addr = getpeername(hClient, (SOCKADDR*)&myaddr, &iClntSize);//이걸 넣으면 지금 자꾸 함수가 그냥 종료됨
 		if (hClient == INVALID_SOCKET) {
-			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			if (WSAGetLastError() == WSAEWOULDBLOCK) {
+				//cout << "non block" << endl;
 				continue;//non block하기위한 부분 block되면 continue해라
+			}
 			break;//block도 아니고 소켓 생성 실패하면 break
 		}
-		cout << "Client Connected" <<inet_ntoa(tClntAddr.sin_addr) << endl;//inet_ntoa도 ip주소를 받아오는데 해당 함수가 getpeername과 차이는?
+		char clientIP[20] = { 0 };
+		if (inet_ntop(AF_INET, &tListenAddr.sin_addr, clientIP, sizeof(clientIP)) == NULL) {
+			cout << "에러" << endl;
+		}
+		cout << "Client Connected" << clientIP << endl;//inet_ntoa도 ip주소를 받아오는데 해당 함수가 getpeername과 차이는?//현재 이 부분 에러 일으킴
 		//recv 부분
 		while (true) {
 			char recvBuffer[1000];
 			int recvLen = recv(hClient, recvBuffer, PACKET_SIZE, 0);
+			//recv(hClient, (char*)&rinfo, sizeof(rinfo), 0);
+			//username = ntohl(rinfo.name);
+			//year = ntohl(rinfo.year);
+			
 			if (recvLen == SOCKET_ERROR) {
 				if (WSAGetLastError() == WSAEWOULDBLOCK) {
 					continue;//non block하기위한 부분 block되면 continue해라
@@ -104,6 +127,7 @@ int main() {
 				}
 				//cout << "send Data: Len" << recvLen << endl;
 				cout << "send Data :" << recvBuffer << endl;
+				//cout << year<< endl;
 				break;
 			}
 		}
