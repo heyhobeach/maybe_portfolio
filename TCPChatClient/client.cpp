fd6a29567cef239374064d55e3a21362260c2636
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-10-31 13:48:28 +0900

    Create client.cpp

---
+++ TCPChatClient/client.cpp
@@ -0,0 +1,327 @@
+#include "pch.h"
+#include <SDKDDKVer.h>
+
+#include <deque>
+#include <iostream>
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+
+#include "C:\Users\zxc12\Desktop\ChatServer\Server\TCPChatServer\TCPChatServer\Project1\Protocol.h"
+
+
+
+class ChatClient
+{
+public:
+	ChatClient(boost::asio::io_context& io_service)
+	: m_IOService(io_service),
+	  m_Socket(io_service)
+	{
+		m_bIsLogin = false;
+		InitializeCriticalSectionAndSpinCount(&m_lock, 4000);
+	}
+
+	~ChatClient()
+	{
+		EnterCriticalSection(&m_lock);
+
+		while( m_SendDataQueue.empty() == false )
+		{
+			delete[] m_SendDataQueue.front();
+			m_SendDataQueue.pop_front();
+		}
+
+		LeaveCriticalSection(&m_lock);
+
+		DeleteCriticalSection(&m_lock); 
+	}
+	
+	bool IsConnecting() { return m_Socket.is_open(); }
+
+	void LoginOK() { m_bIsLogin = true; }//로그인이 확인 되면 OK를 true로
+	bool IsLogin() { return m_bIsLogin; }//로그인 정보가 담겨있는 m_bIsLogin을 넘겨줌 초기값은 false
+
+	void Connect( boost::asio::ip::tcp::endpoint endpoint )
+	{
+		m_nPacketBufferMark = 0;
+
+		m_Socket.async_connect( endpoint,
+								boost::bind(&ChatClient::handle_connect, this,
+									boost::asio::placeholders::error)
+					);
+	}
+
+	void Close()
+	{
+		if( m_Socket.is_open() )
+		{
+			m_Socket.close();
+		}
+	}
+
+	void PostSend( const bool bImmediately, const int nSize, char* pData )
+	{
+		std::cout << "start PostSend" << std::endl;
+		char* pSendData = nullptr;
+		EnterCriticalSection(&m_lock);		// �� ����
+		
+		if( bImmediately == false )
+		{
+			pSendData = new char[nSize];
+			memcpy( pSendData, pData, nSize);
+			
+			m_SendDataQueue.push_back( pSendData );
+		}
+		else
+		{
+			pSendData = pData;
+		}
+
+		if( bImmediately || m_SendDataQueue.size() < 2 )
+		{
+			boost::asio::async_write( m_Socket, boost::asio::buffer( pSendData, nSize ),
+									 boost::bind( &ChatClient::handle_write, this,
+										boost::asio::placeholders::error,
+										boost::asio::placeholders::bytes_transferred )
+									);
+		}
+
+		LeaveCriticalSection(&m_lock);		// �� �Ϸ�
+	}
+
+	
+
+private:
+
+	void PostReceive()
+	{
+		memset( &m_ReceiveBuffer, '\0', sizeof(m_ReceiveBuffer) );
+
+		std::cout << "postReceive" << std::endl;
+
+		m_Socket.async_read_some
+					( 
+						boost::asio::buffer(m_ReceiveBuffer), 
+						boost::bind( &ChatClient::handle_receive, this, 
+													boost::asio::placeholders::error, 
+													boost::asio::placeholders::bytes_transferred ) 
+				
+					);
+	}
+
+	void handle_connect(const boost::system::error_code& error)
+	{
+		if (!error)
+		{	
+			std::cout << "서버 접속 성공" << std::endl;
+			std::cout << "이름을 입력하세요!!" << std::endl;
+
+			PostReceive();
+		}
+		else
+		{
+			std::cout << "서버 접속 실패. error No: " << error.value() << " error Message: " << error.message() << std::endl;
+		}
+	}
+
+	void handle_write(const boost::system::error_code& error, size_t bytes_transferred)
+	{
+		EnterCriticalSection(&m_lock);			// �� ����
+
+		delete[] m_SendDataQueue.front();
+		m_SendDataQueue.pop_front();
+
+		char* pData = nullptr;
+
+		if( m_SendDataQueue.empty() == false )
+		{
+			pData = m_SendDataQueue.front();
+		}
+		
+		LeaveCriticalSection(&m_lock);			// �� �Ϸ�
+
+		
+		if( pData != nullptr )
+		{
+			PACKET_HEADER* pHeader = (PACKET_HEADER*)pData;
+			PostSend( true, pHeader->nSize, pData );
+		}
+	}
+
+	void handle_receive( const boost::system::error_code& error, size_t bytes_transferred )
+	{
+		std::cout << "enter handle_recive" << std::endl;
+		if( error )
+		{
+			std::cout << "handle_recive error" << std::endl;
+			if( error == boost::asio::error::eof )
+			{
+				std::cout << "클라이언트와 연결이 끊어졌습니다" << std::endl;
+			}
+			else 
+			{
+				std::cout << "error No: " << error.value() << " error Message: " << error.message() << std::endl;
+			}
+
+			Close();
+		}
+		else
+		{
+			std::cout << "handle_receive" << std::endl;
+			memcpy( &m_PacketBuffer[ m_nPacketBufferMark ], m_ReceiveBuffer.data(), bytes_transferred );
+		
+			int nPacketData = m_nPacketBufferMark + bytes_transferred;
+			int nReadData = 0;
+		
+			while( nPacketData > 0 )
+			{
+				if( nPacketData < sizeof(PACKET_HEADER) ) 
+				{
+					std::cout << "nPacketData < sizeofe(PACKET_HEADER" << std::endl;
+					break;
+				}
+
+				PACKET_HEADER* pHeader = (PACKET_HEADER*)&m_PacketBuffer[nReadData];
+			
+				if( pHeader->nSize <= nPacketData )
+				{
+					ProcessPacket( &m_PacketBuffer[nReadData] );
+				
+					nPacketData -= pHeader->nSize;
+					nReadData += pHeader->nSize;
+				}
+				else
+				{
+					std::cout << "error" << std::endl;
+					break;
+				}
+			}
+
+			if( nPacketData > 0 )
+			{
+				char TempBuffer[MAX_RECEIVE_BUFFER_LEN] = {0,};
+				memcpy( &TempBuffer[ 0 ], &m_PacketBuffer[nReadData], nPacketData );
+				memcpy( &m_PacketBuffer[ 0 ], &TempBuffer[0], nPacketData );
+			}
+
+			m_nPacketBufferMark = nPacketData;
+
+
+			PostReceive(); 
+		}
+	}
+
+	void ProcessPacket( const char*pData )
+	{
+		PACKET_HEADER* pheader = (PACKET_HEADER*)pData;
+		std::cout << "ProcessPacket" << std::endl;
+		switch( pheader->nID )
+		{
+			std::cout << "switch" << std::endl;
+		case RES_IN:
+			{
+				PKT_RES_IN* pPacket = (PKT_RES_IN*)pData;
+				
+				LoginOK();
+				
+				std::cout << "클라이언트 로그인 성공 ?: " << pPacket->bIsSuccess << std::endl; 
+			}
+			break;
+		case NOTICE_CHAT:
+			{
+				PKT_NOTICE_CHAT* pPacket = (PKT_NOTICE_CHAT*)pData;
+
+				std::cout << pPacket->szName << ": " << pPacket->szMessage << std::endl; 
+			}
+			break;
+		default: {
+			std::cout << "notingh" << std::endl;
+		}
+		}
+	}
+ 
+	  
+
+private:
+	boost::asio::io_context& m_IOService;
+	boost::asio::ip::tcp::socket m_Socket;
+
+	std::array<char, 512> m_ReceiveBuffer;
+	
+	int m_nPacketBufferMark;
+	char m_PacketBuffer[MAX_RECEIVE_BUFFER_LEN*2];
+
+	CRITICAL_SECTION m_lock;
+	std::deque< char* > m_SendDataQueue;
+
+	bool m_bIsLogin;
+};
+
+
+int main()
+{
+	boost::asio::io_context io_service;
+
+	auto endpoint = boost::asio::ip::tcp::endpoint( 
+						boost::asio::ip::make_address("127.0.0.1"),
+						PORT_NUMBER);
+
+	ChatClient Cliet( io_service );
+	Cliet.Connect( endpoint );
+
+	boost::thread thread( boost::bind(&boost::asio::io_context::run, &io_service) );//io_context.run을 실행을 스레드로 돌림
+		
+
+	char szInputMessage[MAX_MESSAGE_LEN * 2] = {0,};
+	
+	while( std::cin.getline( szInputMessage, MAX_MESSAGE_LEN) )
+	{
+		std::cout << "szInputMessage  " << szInputMessage <<"  length : "<<strnlen_s(szInputMessage, MAX_MESSAGE_LEN) << std::endl;
+
+		if( strnlen_s( szInputMessage, MAX_MESSAGE_LEN ) == 0 )
+		{
+			size_t len = strnlen_s(szInputMessage, MAX_MESSAGE_LEN);
+			std::cout <<"szInputMessage  " << szInputMessage << std::endl;
+			std::cout <<len <<std::endl;
+			std::cout << "getline error" << std::endl;
+			break;
+		}
+
+		if( Cliet.IsConnecting() == false )
+		{
+			std::cout << "서버와 연결되지 않았습니다" << std::endl;
+			continue;
+		}
+
+		if( Cliet.IsLogin() == false )//로그인 되어있지 않을때
+		{
+			std::cout << "로그인 되어있지 않음" << std::endl;
+			PKT_REQ_IN SendPkt;
+			SendPkt.Init();
+			strncpy_s( SendPkt.szName, MAX_NAME_LEN, szInputMessage, MAX_NAME_LEN-1 );
+
+			Cliet.PostSend( false, SendPkt.nSize, (char*)&SendPkt );
+		}
+		else
+		{
+			std::cout << "로그인 되어있음 채팅 데이터 전송" << std::endl;
+			PKT_REQ_CHAT SendPkt;
+			SendPkt.Init();
+			strncpy_s( SendPkt.szMessage, MAX_MESSAGE_LEN, szInputMessage, MAX_MESSAGE_LEN-1 );
+
+			Cliet.PostSend( false, SendPkt.nSize, (char*)&SendPkt );
+		}
+	}
+
+	io_service.stop();
+
+	Cliet.Close();
+	
+	thread.join();
+  
+	std::cout << "클라이언트를 종료해 주세요" << std::endl;
+
+	return 0;
+}
